f85a9317897e05203e45f457bab29ef313cdffa3
lakshiliyanagamage <lakshiliyanagamage@gmail.com>
2021-03-16 01:50:27 +0530

    new.py
    new

---
+++ app/src/main/python/new.py
@@ -0,0 +1,22 @@
+import pyttsx3
+import speech_recognition as sr
+
+
+r=sr.Recognizer()  
+with sr.Microphone() as source:
+    print("say something");
+    audio=r.listen(source)
+    print("time over")
+try:
+    print("text : "+r.recognize_google(audio));
+    c=r.recognize_google(audio)
+except:
+    pass;
+
+# initialisation 
+engine = pyttsx3.init() 
+  
+# testing 
+engine.say(c) 
+engine.say("Thank you") 
+engine.runAndWait() 
