c0d6881aabf1ba2c934a2694d72125a8ebe43316
Hashan madusanka <61758181+Hashan9625@users.noreply.github.com>
2022-01-03 18:32:22 +0530

    json read

--- app/src/main/python/ai.py
+++ app/src/main/python/ai.py
@@ -1,2 +1,21 @@
+# from nltk.stem.api import StemmerI
+# import nltk
+# import numpy as np
+# import pickle
+from os.path import dirname, join
+import json
+# from ntlk.chat.util import Chat
 def main():
-    return "hashan"
+    filename = join(dirname(__file__),'model/amita.json')
+
+    intents = json.loads(open(filename).read())
+    #     words = pickle.load(open('model/words.pkl', 'rb'))
+    print("-------------------------------"+str(intents))
+    return "  hhh " + str(intents)
+
+
+
+
+# print(main())
+# with open(filename, 'r', encoding='utf8', errors="ignore") as fin:
+# data = fin.read().lower()
